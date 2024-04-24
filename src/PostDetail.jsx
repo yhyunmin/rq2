54573907d10ec399fb3c3a81cbb8347ebce24258
smurf <smurf@kakao.com>
2024-04-24 22:08:23 +0900

    0424

--- src/PostDetail.jsx
+++ src/PostDetail.jsx
@@ -1,17 +1,36 @@
-import { fetchComments } from "./api";
-import "./PostDetail.css";
+import { fetchComments } from './api';
+import './PostDetail.css';
 
+// 코멘트가 업데이트 안되는 이유
+// 코멘트라는 쿼리키를 모든postDetail에서 사용중
+//  이럴땐 쿼리키를 트리거를 활용해 업데이트해야함
+//
+// postID 데이터를 갖고있으므로 쿼리마다 캐시할 수 있음
+// 각포스테에대한 쿼리를 별도로 라벨링 할 수있음
+//  퀴리키 에서 두번째 요소로, 키가 변경될떄마다 어떻게 처리할지 보여줌
+// 이경우 post.id 가 바뀔때마다 stale시간과 캐시시간을  갖게됨
+// 이때문에 데이터를 가져올 떄 쓰는 쿼리 함수의 모든 값이 키의 일부여야함
 export function PostDetail({ post }) {
   // replace with useQuery
-  const data = [];
+  // const data = [];
+  const { data, isError, isLoading, error } = useQuery({
+    queryKey: ['comments', post.id],
+    queryFn: () => fetchComments(post.id),
+  });
 
+  if (isLoading) {
+    return <h3>Loading...</h3>;
+  }
+  if (isError) {
+    return <h3>{error.toString()}</h3>;
+  }
   return (
     <>
-      <h3 style={{ color: "blue" }}>{post.title}</h3>
+      <h3 style={{ color: 'blue' }}>{post.title}</h3>
       <button>Delete</button> <button>Update title</button>
       <p>{post.body}</p>
       <h4>Comments</h4>
-      {data.map((comment) => (
+      {data.map(comment => (
         <li key={comment.id}>
           {comment.email}: {comment.body}
         </li>
--- src/Posts.jsx
+++ src/Posts.jsx
@@ -1,17 +1,42 @@
-import { useState } from "react";
-import { useQuery } from "@tanstack/react-query";
+import { useEffect, useState } from 'react';
+import { useQuery, useQueryClient } from '@tanstack/react-query';
 
-import { fetchPosts, deletePost, updatePost } from "./api";
-import { PostDetail } from "./PostDetail";
+import { fetchPosts, deletePost, updatePost } from './api';
+import { PostDetail } from './PostDetail';
 const maxPostPage = 10;
 
+// 프리패치
+// 데이터를 캐시에 넣어놓음
+// 데이터는 stale로 간주
+// 데이터를 사용해야할떄 stale이라
+// 캐시에있는 데이터를 제공, 새로고침될떄까지 캐시에있는 데이터를 표시할 수 있음
+// 프리페칭은 페이지네이션뿐 아니라 모든곳 사용가능
+//
+// isFetching vs isLoading
+//  isFetching 비동기 쿼리 함수가 해결되지않앗을때 true
+// isLoading 캐시된 데이터가 없음 + fetching 중
 export function Posts() {
-  const [currentPage, setCurrentPage] = useState(0);
+  const [currentPage, setCurrentPage] = useState(1);
   const [selectedPost, setSelectedPost] = useState(null);
 
+  //프리패칭 하는 훅
+  const queryClient = useQueryClient();
+
+  useEffect(() => {
+    if (currentPage < maxPostPage) {
+      // 미리 가져오려는 데이터
+      const nextPage = currentPage + 1;
+      //프리패칭 하는 함수
+      queryClient.prefetchQuery({
+        queryKey: ['posts', nextPage],
+        queryFn: () => fetchPosts(nextPage),
+      });
+    }
+  }, [currentPage, queryClient]);
+
   const { data, isError, error, isLoading } = useQuery({
-    queryKey: ["posts"],
-    queryFn: fetchPosts,
+    queryKey: ['posts', currentPage],
+    queryFn: () => fetchPosts(currentPage),
     staleTime: 2000, // 2 seconds
   });
   if (isLoading) {
@@ -29,22 +54,29 @@ export function Posts() {
   return (
     <>
       <ul>
-        {data.map((post) => (
+        {data.map(post => (
           <li
             key={post.id}
-            className="post-title"
-            onClick={() => setSelectedPost(post)}
-          >
+            className='post-title'
+            onClick={() => setSelectedPost(post)}>
             {post.title}
           </li>
         ))}
       </ul>
-      <div className="pages">
-        <button disabled onClick={() => {}}>
-          Previous page
-        </button>
+      <div className='pages'>
+        <button
+          disabled={currentPage <= 1}
+          onClick={() => {
+            setCurrentPage(prev => prev - 1);
+          }}
+          Previous
+          page></button>
         <span>Page {currentPage + 1}</span>
-        <button disabled onClick={() => {}}>
+        <button
+          disabled={currentPage >= maxPostPage}
+          onClick={() => {
+            setCurrentPage(prev => prev + 1);
+          }}>
           Next page
         </button>
       </div>
